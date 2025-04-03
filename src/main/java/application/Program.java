8dc50934a9b51fdc067ff9bf9e2a6122f2150c4f
Giovanni <gi.canella85@outlook.com>
2025-04-02 22:13:35 -0300

    delete implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -7,22 +7,53 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
         //O metodo createSellerDao() da DaoFactory retorna um objeto do tipo SellerDao.
         SellerDao sellerDao = DaoFactory.createSellerDao(); //Macete para nao expor a implementacao, somente a interface. Uma forma de fazer a injecao de dependencia sem mostrar a implementacao
 
         System.out.println("=== TEST 1: seller findById =====");
         Seller seller = sellerDao.findById(3);
 
-        System.out.println("\n=== TEST 1: seller findById =====");
+        System.out.println("\n=== TEST 2: seller findByDepartment =====");
         Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
         // Para cada Seller obj na minha lista list eu imprimo esse obj
         for (Seller obj : list) {
             System.out.println(obj);
         }
+
+        System.out.println("\n=== TEST 1: seller findAll =====");
+        list = sellerDao.findAll();
+        // Para cada Seller obj na minha lista list eu imprimo esse obj
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+
+        System.out.println("\n=== TEST 1: seller insert =====");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + seller.getId());
+
+        System.out.println("\n=== TEST 5: seller update =====");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
+
+        System.out.println("\n=== TEST 6: seller delete =====");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed");
+
+        sc.close();
+
 
     }
 }
--- src/main/java/model/dao/impl/SellerDaoJDBC.java
+++ src/main/java/model/dao/impl/SellerDaoJDBC.java
@@ -6,10 +6,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.*;
 
 public class SellerDaoJDBC implements SellerDao {
@@ -22,17 +19,87 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void insert(Seller obj) {
+        PreparedStatement st = null;
 
+        try {
+            st = conn.prepareStatement("INSERT INTO seller\n" +
+                    "(Name, Email, BirthDate, BaseSalary, DepartmentId)\n" +
+                    "VALUES\n" +
+                    "(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS); //retorna tbm o id do vendedor inserido
+
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+            st.setDouble(4, obj.getBaseSalary());
+            st.setInt(5, obj.getDepartment().getId());
+
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                if (rs.next()) {
+                    int id = rs.getInt(1); //O metodo getGeneratedKeys() pega o ID gerado automaticamente pelo banco de dados após um INSERT. Esse ID geralmente é a chave primária com AUTO_INCREMENT.
+                    obj.setId(id);
+                }
+                DB.closeResultSet(rs);
+            } else {
+                throw new DbException("Unexpected error! No rows affected");
+            }
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+
+        finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
     public void update(Seller obj) {
+        PreparedStatement st = null;
 
+        try {
+            st = conn.prepareStatement("UPDATE seller\n" +
+                    "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ?\n" +
+                    "WHERE Id = ?");
+
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+            st.setDouble(4, obj.getBaseSalary());
+            st.setInt(5, obj.getDepartment().getId());
+            st.setInt(6, obj.getId());
+
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+
+        finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
     public void deleteById(int id) {
+        PreparedStatement st = null;
 
+        try {
+            st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+
+            st.setInt(1, id);
+
+            st.executeUpdate();
+
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+
+        finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
@@ -110,8 +177,63 @@ public class SellerDaoJDBC implements SellerDao {
     @Override
     public List<Seller> findAll() {
 
-        return List.of();
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName\n" +
+                            "FROM seller INNER JOIN department\n" +
+                            "ON seller.DepartmentId = department.Id\n" +
+                            "ORDER BY Name");
+
+
+            rs = st.executeQuery(); //Aqui vai retornar o numero de linhas afetadas pela query
+
+            //Temos que usar o while pq pode ter 0 ou mais resultados, entao eu quero que
+            //percorra meu resultSet ENQUANTO tiver um proximo
+
+            List<Seller> list = new ArrayList<>();
+
+            //Criei um map vazio e eu coloquei como chave um int e valor o Department
+            //Eu vou guardar dentro desse map qualquer departamento que eu instanciar
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (rs.next()) {
+
+                //Cada vez que passar no meu while eu vou ter que ver se o departamento existe, como?
+                //O map.get tenta buscar um departamento que tem esse Id passado como parametro, se nao existir
+                //esse map.get vai retornar null e se for null ai sim eu vou instanciar o departamento
+                Department dep = map.get(rs.getInt("DepartmentId")); //Pega o id da coluna DepartmentId e armazena no map o objeto desse Department com esse id
+
+                // Se nao existir esse dep, eu vou mandar instanciar esse departamento a partir do resultSet
+                if (dep == null) {
+                    dep = instanciateDepartment(rs);
+
+                    //agora eu vou salvar esse departamento dentro do meu map pra da proxima vez eu consigo verificar ele e ver que ele ja existe
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+
+
+                Seller obj = instanciateSeller(rs, dep); //Assim garantimos que caso o departamento seja o mesmo o seller aponta para o mesmo objeto Department
+                list.add(obj);
+
+                //Incluindo algum controle para nao incluir mais de um dep pq os Seller estao em apenas um Departamento. Vamos fazer isso usando o map
+            }
+
+            return list;
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+
+        finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+
     }
+
 
     @Override
     public List<Seller> findByDepartment(Department department) {
@@ -144,7 +266,7 @@ public class SellerDaoJDBC implements SellerDao {
                 //Cada vez que passar no meu while eu vou ter que ver se o departamento existe, como?
                 //O map.get tenta buscar um departamento que tem esse Id passado como parametro, se nao existir
                 //esse map.get vai retornar null e se for null ai sim eu vou instanciar o departamento
-                Department dep = map.get(rs.getInt("DepartmentId"));
+                Department dep = map.get(rs.getInt("DepartmentId")); //Pega o id da coluna DepartmentId e armazena no map o objeto desse Department com esse id
 
                 // Se nao existir esse dep, eu vou mandar instanciar esse departamento a partir do resultSet
                 if (dep == null) {
@@ -155,7 +277,7 @@ public class SellerDaoJDBC implements SellerDao {
                 }
 
 
-                Seller obj = instanciateSeller(rs, dep);
+                Seller obj = instanciateSeller(rs, dep); //Assim garantimos que caso o departamento seja o mesmo o seller aponta para o mesmo objeto Department
                 list.add(obj);
 
                 //Incluindo algum controle para nao incluir mais de um dep pq os Seller estao em apenas um Departamento. Vamos fazer isso usando o map
